2bea08229c1369d3d34029a1e70071db24782c9c
ctabo91 <ctaborda91@gmail.com>
2024-03-25 20:58:34 -0400

    modified NavBar to be more mobile friendly, and fixed some typos

--- src/routes-nav/NavBar.js
+++ src/routes-nav/NavBar.js
@@ -1,6 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import UserContext from "../auth/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNavicon } from "@fortawesome/free-solid-svg-icons";
 import "./NavBar.css";
 
 
@@ -12,8 +14,17 @@ import "./NavBar.css";
 
 function NavBar({ logout }) {
     const { currentUser } = useContext(UserContext);
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-    const loggeInNav = () => {
+    const toggleDropdown = () => {
+        setIsNavbarOpen(!isNavbarOpen);
+    };
+
+    const closeNavbar = () => {
+        setIsNavbarOpen(false);
+    };
+
+    const loggedInNav = () => {
         return (
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item dropdown mr-4">
@@ -30,22 +41,22 @@ function NavBar({ logout }) {
                     </NavLink>
                     <div className="dropdown-menu" aria-labelledby="recipesDropdown">
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/meals">
+                            <NavLink className="nav-link pr-0" to="/recipes/meals" onClick={closeNavbar}>
                                 Meals
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/drinks">
+                            <NavLink className="nav-link pr-0" to="/recipes/drinks" onClick={closeNavbar}>
                                 Drinks
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/meals/categories">
+                            <NavLink className="nav-link pr-0" to="/recipes/meals/categories" onClick={closeNavbar}>
                                 Meal Categories
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/drinks/categories">
+                            <NavLink className="nav-link pr-0" to="/recipes/drinks/categories" onClick={closeNavbar}>
                                 Drink Categories
                             </NavLink>
                         </div>
@@ -65,34 +76,34 @@ function NavBar({ logout }) {
                     </NavLink>
                     <div className="dropdown-menu" aria-labelledby="personalDropdown">
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/create/meals">
+                            <NavLink className="nav-link pr-0" to="/recipes/create/meals" onClick={closeNavbar}>
                                 Create Meal
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/create/drinks">
+                            <NavLink className="nav-link pr-0" to="/recipes/create/drinks" onClick={closeNavbar}>
                                 Create Drink
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/meals/personal">
+                            <NavLink className="nav-link pr-0" to="/recipes/meals/personal" onClick={closeNavbar}>
                                 Personal Meals
                             </NavLink>
                         </div>
                         <div className="dropdown-item-container">
-                            <NavLink className="nav-link pr-0" to="/recipes/drinks/personal">
+                            <NavLink className="nav-link pr-0" to="/recipes/drinks/personal" onClick={closeNavbar}>
                                 Personal Drinks
                             </NavLink>
                         </div>
                     </div>
                 </li>
                 <li className="nav-item mr-4">
-                    <NavLink className="nav-link" to="/profile">
+                    <NavLink className="nav-link" to="/profile" onClick={closeNavbar}>
                         Profile
                     </NavLink>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/" onClick={logout}>
+                    <Link className="nav-link" to="/" onClick={() => { logout(); closeNavbar(); }}>
                         Logout
                     </Link>
                 </li>
@@ -104,12 +115,12 @@ function NavBar({ logout }) {
         return (
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item mr-4">
-                    <NavLink className="nav-link" to="/login">
+                    <NavLink className="nav-link" to="/login" onClick={closeNavbar}>
                         Login
                     </NavLink>
                 </li>
                 <li className="nav-item mr-4">
-                    <NavLink className="nav-link" to="/signup">
+                    <NavLink className="nav-link" to="/signup" onClick={closeNavbar}>
                         Sign Up
                     </NavLink>
                 </li>
@@ -118,16 +129,23 @@ function NavBar({ logout }) {
     };
 
     return (
-        <div className="container">
-            <nav className="NavBar navbar navbar-expand-lg d-flex justify-content-between">
-                <Link className="navbar-brand ml-2 pb-0 pt-0" to="/">
-                    DreamHost
-                </Link>
-                <div className="collapse navbar-collapse">
-                    {currentUser ? loggeInNav() : loggedOutNav()}
-                </div>
-            </nav>
-        </div>
+        <nav className="NavBar navbar navbar-expand-lg d-flex justify-content-between">
+            <Link className="navbar-brand ml-2 pb-0 pt-0" to="/" onClick={closeNavbar}>
+                DreamHost
+            </Link>
+            <button
+                className="navbar-toggler"
+                type="button"
+                onClick={toggleDropdown}
+                aria-expanded={isNavbarOpen ? "true" : "false"}
+                aria-label="Toggle navigation"
+            >
+                <FontAwesomeIcon icon={faNavicon} className="navbar-toggler-icon" style={{color: 'rgb(110, 66, 66)'}} />
+            </button>
+            <div className={`collapse navbar-collapse ${isNavbarOpen ? "show" : ""}`}>
+                {currentUser ? loggedInNav() : loggedOutNav()}
+            </div>
+        </nav>
     );
 }
 
